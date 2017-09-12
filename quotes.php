58ac255e8471185b3fc8dc74e8f05b4a87c51a52
Gregory Coleman <gregory.m.coleman@gmail.com>
2017-09-12 17:48:33 -0500

    Split in two

---
+++ quotes.php
@@ -0,0 +1,52 @@
+<?php
+
+// Dilbert Quotes
+$quote_array = array(
+    "I say no to alcohol, it just doesn't listen.",
+    "A friend in need is a pest indeed.",
+    "Marriage is one of the chief causes of divorce.",
+    "Work is fine if it doesn't take too much of your time.",
+    "When everything comes in your way you're in the wrong lane.",
+    "The light at the end of the tunnel may be an incoming train...",
+    "Born free, taxed to death.",
+    "Everyone has a photographic memory, some just don't have film.",
+    "Life is unsure; always eat your dessert first.",
+    "Smile, it makes people wonder what you are thinking.",
+    "If you keep your feet firmly on the ground, you'll have trouble putting on your pants.",
+    "It's not hard to meet expenses, they are everywhere.",
+    "I love being a writer... what I can't stand is the paperwork.",
+    "A printer consists of 3 main parts: the case, the jammed paper tray and the blinking red light.",
+    "The guy who invented the first wheel was an idiot. The guy who invented the other three, he was the genius.",
+    "The trouble with being punctual is that no one is there to appreciate it.",
+    "In a country of free speech, why are there phone bills?",
+    "If you cannot change your mind, are you sure you have one?",
+    "Beat the 5 O'clock rush, leave work at noon!",
+    "If you can't convince them, confuse them.",
+    "It's not the fall that kills you. It's the sudden stop at the end.",
+    "I couldn't repair your brakes, so I made your horn louder.",
+    "Hot glass looks same as cold glass. - Cunino's Law of Burnt Fingers",
+    "The cigarette does the smoking you are just the sucker.",
+    "Someday is not a day of the week",
+    "Whenever I find the key to success, someone changes the lock.",
+    "To Err is human, to forgive is not a Company policy.",
+    "The road to success.... Is always under construction.",
+    "Alcohol doesn't solve any problems, but if you think again, neither does Milk.",
+    "In order to get a Loan, you first need to prove that you don't need it.",
+    "Why do ghosts wear pants?",
+    "There’s no kill switch on awesome.",
+    "Never mind. My phone took care of it.",
+    "Did you learn to debate on the Internet?",
+    "Self-respect is like a prison for the soul.",
+    "Goals are a form of self-inflicted slavery.",
+    "I’m not lazy, I’m useless. There’s a big difference.",
+    "With any luck, your soul mate won’t be perceptive.",
+    "I can no longer resist the urge to text while you talk.",
+    "You might want to pick a defense that’s less checkable.",
+    "I’m toying with the idea of becoming a useful member of society.",
+    "I tried to read your email but the signal-to-noise ratio was too low.",
+    "I’ve decided to be more aggressive in blaming others for my lack of success."
+   );
+
+    $quote = $quote_array[mt_rand(0, count($quote_array) - 1)]; // Mersenne Twister
+
+?>
